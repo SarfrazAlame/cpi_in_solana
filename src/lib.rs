c2466e0999864b352e929aba65f8e14bf94c8003
sarfraz alam <sarfrazk7858@gmail.com>
2025-08-09 11:33:42 +0530

    client

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,32 @@
 use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::{
     account_info::{AccountInfo, next_account_info},
-    entrypoint::{self, ProgramResult},
+    entrypoint,
+    entrypoint::ProgramResult,
     pubkey::Pubkey,
 };
 
 entrypoint!(process_instruction);
 
 #[derive(BorshDeserialize, BorshSerialize)]
-
 struct OnChainData {
     count: u32,
 }
 
-fn process_instruction(
-    program_id: &Pubkey,
+pub fn process_instruction(
+    program_Id: &Pubkey,
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
     let mut iter = accounts.iter();
     let data_account = next_account_info(&mut iter)?;
 
-    let mut counter = OnChainData::try_from_slice(&data_account.data.borrow_mut())?;
+    let mut counter = OnChainData::try_from_slice(&mut data_account.data.borrow_mut())?;
 
     if counter.count == 0 {
         counter.count = 1;
-    } else {
-        counter.count = counter.count * 2;
     }
+    counter.count = counter.count * 2;
 
     counter.serialize(&mut *data_account.data.borrow_mut());
 
